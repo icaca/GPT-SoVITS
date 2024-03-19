ad11c1dff7046dfbae221fcc34812e37acbc7ca4
caca <icaca@live.com>
2024-03-19 19:26:11 +0800

    Update inference_webui.py

--- GPT_SoVITS/inference_webui.py
+++ GPT_SoVITS/inference_webui.py
@@ -313,8 +313,8 @@ def merge_short_text_in_array(texts, threshold):
 
 
 def get_tts_wav(ref_wav_path, prompt_text, prompt_language, text, text_language, how_to_cut=i18n("不切"), top_k=20, top_p=0.6, temperature=0.6, ref_free=False, so="", gpt=""):
-    change_sovits_weights(so)
-    change_gpt_weights(gpt)
+    change_sovits_weights([so])
+    change_gpt_weights([gpt])
     if prompt_text is None or len(prompt_text) == 0:
         ref_free = True
     t0 = ttime()
@@ -598,7 +598,7 @@ with gr.Blocks(title="GPT-SoVITS WebUI") as app:
 
         inference_button.click(
             get_tts_wav,
-            [inp_ref, prompt_text, prompt_language, text, text_language, how_to_cut, top_k, top_p, temperature, ref_text_free,text_so,text_gpt],
+            [inp_ref, prompt_text, prompt_language, text, text_language, how_to_cut, top_k, top_p, temperature, ref_text_free, text_so, text_gpt],
             [output],
         )
 
